27ce52b0b5cece901b92aae4ab21285933898f66
karnika.indras <karnika.indras@YI1007112DT.Yash.com>
2018-04-24 12:19:45 +0530

    added test cases

--- userapplicationdevelopmentdemo/src/test/java/com/yash/userapp/UserServiceTest.java
+++ userapplicationdevelopmentdemo/src/test/java/com/yash/userapp/UserServiceTest.java
@@ -1,21 +1,37 @@
 package com.yash.userapp;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.yash.userapp.daoimpl.UserDaoImpl;
 import com.yash.userapp.pojo.User;
-import com.yash.userapp.service.UserService;
 import com.yash.userapp.serviceimpl.UserServiceImpl;
+
 
 public class UserServiceTest {
 
-	public static void main(String[] args) {
-		User user=new User();
-		user.setId(101);
-		user.setName("TestUser");
-		user.setSalary(2000.00);
-		
-		UserService userService=new UserServiceImpl();
-		userService.addUser(user);
-		
-
+	@Mock
+	private UserDaoImpl userdao;
+	
+	@InjectMocks
+	private UserServiceImpl userService;
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
 	}
 
+	@Test
+	public void test_addUser_method_with_user_object_called_once() {
+		User user = new User();
+		userService.addUser(user);
+		verify(userdao,times(1)).insertUser(user);
+	}
 }
+
